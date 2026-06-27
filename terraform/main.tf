7f8b7e1f63d1103911b4cf6af55af63878b91f32
yoanlouvois <louvois45@gmail.com>
2026-06-28 01:30:01 +0200

    Add Terraform modules in the main

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+module "network" {
+  source = "./modules/network"
+}
+
+module "security" {
+  source = "./modules/security"
+
+  vpc_id = module.network.vpc_id
+}
+
+module "registry" {
+  source = "./modules/registry"
+}
+
+module "compute" {
+  source = "./modules/compute"
+}
+
+module "load_balancing" {
+  source = "./modules/load_balancing"
+}
+
+module "secrets" {
+  source = "./modules/secrets"
+}
+
+module "storage" {
+  source = "./modules/storage"
+}
