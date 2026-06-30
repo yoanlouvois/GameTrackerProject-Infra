6f013423c973b408d6bffaccd46d4c9042c213c3
yoanlouvois <louvois45@gmail.com>
2026-06-30 11:47:34 +0200

    Add output variables for front and back repository URLs and ARNs

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "front_repository_url" {
+  value = module.registry.front_repository_url
+}
+
+output "back_repository_url" {
+  value = module.registry.back_repository_url
+}
+
+output "front_repository_arn" {
+  value = module.registry.front_repository_arn
+}
+
+output "back_repository_arn" {
+  value = module.registry.back_repository_arn
+}
